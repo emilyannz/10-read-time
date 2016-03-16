877b3fccee3cfdeb4aa3f0d3599b78a98617c48d
Emily Znamierowski <emilyannz@gmail.com>
2016-03-16 18:20:24 -0500

    show read time for articles to user complete

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 'use strict';
 /* globals numWords, readTime */
+
+
+const entryinfos = document.querySelectorAll(`.entry__info`);
+const entries = document.querySelectorAll(`.entry__content`);
+
+for (let i = 0; i < entryinfos.length; i++) {
+  const readtime = document.createElement(`li`);
+  readtime.classList.add(`read-time`);
+  entryinfos[i].appendChild(readtime);
+
+  readtime.innerText = `${readTime(entries[i].innerText, 200)} mins`;
+}
+// find text in article, use text in readtime, and then show to user
--- src/read-time.js
+++ src/read-time.js
@@ -1,5 +1,7 @@
 'use strict';
 
-function readTime(string){
 
+function readTime(string, wpm = 1) {
+
+  return Math.ceil(numWords(string)/wpm);
 }
