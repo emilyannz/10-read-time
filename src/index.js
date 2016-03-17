1175bd742fa62ba858fedf1fa4648536f8c92b95
Emily Znamierowski <emilyannz@gmail.com>
2016-03-16 20:02:09 -0500

    all tests complete

--- src/index.js
+++ src/index.js
@@ -2,14 +2,27 @@
 /* globals numWords, readTime */
 
 
+const entries = document.querySelectorAll(`.entry`);
 const entryinfos = document.querySelectorAll(`.entry__info`);
-const entries = document.querySelectorAll(`.entry__content`);
+const posts = document.querySelectorAll(`.entry__content`);
+const bodyEl = document.querySelector(`body`);
+const postTitles = document.querySelectorAll(`.entry__title`);
+
+const nav = document.createElement(`nav`);
+bodyEl.appendChild(nav);
 
 for (let i = 0; i < entryinfos.length; i++) {
+  const navlink = document.createElement(`a`);
+  nav.appendChild(navlink);
+
+  navlink.setAttribute(`href`, `#${entries[i].id}`);
+  navlink.innerText = `${postTitles[i].innerText}`;
+
   const readtime = document.createElement(`li`);
   readtime.classList.add(`read-time`);
   entryinfos[i].appendChild(readtime);
-
-  readtime.innerText = `${readTime(entries[i].innerText, 200)} mins`;
-}
 // find text in article, use text in readtime, and then show to user
+  readtime.innerText = `${readTime(posts[i].innerText, 200)} mins`;
+}
+
+// create nav element and append it to the body
